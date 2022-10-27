7441f09763de13921ec2d53e592f99197e318a5a
BK <you@example.com>
2022-10-27 19:45:27 +0400

    get-albums-with-person edited && app-user/get-albums-thumbnail-icons added

--- controllers/appUserController/telegramController/telegramController.ts
+++ controllers/appUserController/telegramController/telegramController.ts
@@ -1,23 +1,23 @@
 import { Request, Response } from 'express';
 
-// import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from 'node-telegram-bot-api';
 
-// const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
+const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
 
 class TelegramController {
   generateOTP(req: Request, res: Response): void {
-    // interface Phone {
-    //   phone: string
-    // }
-    // const { phone }: Phone = req.body;
+    interface Phone {
+      phone: string
+    }
+    const { phone }: Phone = req.body;
 
     const OTP = `${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
 
     try {
-      // bot.sendMessage(
-      //   Number(process.env.TB_BOT_GROUP_CHAT_ID),
-      //   `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
-      // );
+      bot.sendMessage(
+        Number(process.env.TB_BOT_GROUP_CHAT_ID),
+        `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
+      );
       res.json({ OTP });
     } catch (e) {
       console.log(e);
@@ -26,7 +26,7 @@ class TelegramController {
 
   sendPhotoNotification(req: Request, res: Response) {
     try {
-      // bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
+      bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
 
       res.send();
     } catch (e) {
