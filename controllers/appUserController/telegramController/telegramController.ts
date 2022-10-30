b9af960beb81402d95c5c425fc7ce7f0cca8ef9f
BK <you@example.com>
2022-10-30 14:35:30 +0400

    photoController minor fixes 1.33.

--- controllers/appUserController/telegramController/telegramController.ts
+++ controllers/appUserController/telegramController/telegramController.ts
@@ -1,62 +1,23 @@
-import { Request, Response } from 'express';
-
-// import TelegramBot from 'node-telegram-bot-api';
-
-// const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
-
-class TelegramController {
-  generateOTP(req: Request, res: Response): void {
-    // interface Phone {
-    //   phone: string
-    // }
-    // const { phone }: Phone = req.body;
-
-    const OTP = `${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
-
-    try {
-      // bot.sendMessage(
-      //   Number(process.env.TB_BOT_GROUP_CHAT_ID),
-      //   `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
-      // );
-      res.json({ OTP });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  sendPhotoNotification(req: Request, res: Response) {
-    try {
-      // bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
-
-      res.send();
-    } catch (e) {
-      console.log(e);
-    }
-  }
-}
-
-export default new TelegramController();
-
 // import { Request, Response } from 'express';
 
-// import TelegramBot from 'node-telegram-bot-api';
+// // import TelegramBot from 'node-telegram-bot-api';
 
-// const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
+// // const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
 
 // class TelegramController {
 //   generateOTP(req: Request, res: Response): void {
-//     interface Phone {
-//       phone: string
-//     }
-//     const { phone }: Phone = req.body;
+//     // interface Phone {
+//     //   phone: string
+//     // }
+//     // const { phone }: Phone = req.body;
 
 //     const OTP = `${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
 
 //     try {
-//       bot.sendMessage(
-//         Number(process.env.TB_BOT_GROUP_CHAT_ID),
-//         `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
-//       );
+//       // bot.sendMessage(
+//       //   Number(process.env.TB_BOT_GROUP_CHAT_ID),
+//       //   `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
+//       // );
 //       res.json({ OTP });
 //     } catch (e) {
 //       console.log(e);
@@ -65,7 +26,7 @@ export default new TelegramController();
 
 //   sendPhotoNotification(req: Request, res: Response) {
 //     try {
-//       bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
+//       // bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
 
 //       res.send();
 //     } catch (e) {
@@ -75,3 +36,42 @@ export default new TelegramController();
 // }
 
 // export default new TelegramController();
+
+import { Request, Response } from 'express';
+
+import TelegramBot from 'node-telegram-bot-api';
+
+const bot = new TelegramBot(`${process.env.TELEGRAM_BOT_KEY!}`, { polling: true });
+
+class TelegramController {
+  generateOTP(req: Request, res: Response): void {
+    interface Phone {
+      phone: string
+    }
+    const { phone }: Phone = req.body;
+
+    const OTP = `${Math.floor(Math.random() * (999999 - 100000) + 100000)}`;
+
+    try {
+      bot.sendMessage(
+        Number(process.env.TB_BOT_GROUP_CHAT_ID),
+        `Your phone is: ${phone}\nYour OTP is: ${OTP}`,
+      );
+      res.json({ OTP });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  sendPhotoNotification(req: Request, res: Response) {
+    try {
+      bot.sendMessage(Number(process.env.TB_BOT_GROUP_CHAT_ID), 'PhotoDrop: your photos have dropped🔥\n\nCheck the out here:\n https://userAppUrlWillBeSoonHere.com');
+
+      res.send();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+export default new TelegramController();
